f09d13b2fe054d6ae5706b504b5c4285d60c7889
Ashvin Coca <AshvinCoca@gmail.com>
2019-02-14 23:46:41 -0800

    Set up and (properly) configured Jest/Enzyme testing framework

--- client/components/tests/__tests__/App.test.jsx
+++ client/components/tests/__tests__/App.test.jsx
@@ -1,13 +1,6 @@
 import { render } from 'enzyme';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
 import React from 'react';
-import App from '../App.jsx';
-
-
-configure({ adapter: new Adapter() });
-
+import App from '../../App.jsx';
 
 
 describe('App Component', () => {
